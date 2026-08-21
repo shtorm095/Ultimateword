938524d88b6b527ddb66a05cfd3f7190b950a4d2
shtorm095 <shtorm095@gmail.com>
2026-08-22 01:57:40 +0200

    Make Audio local-only patch root compatible with v1.6.7 build

--- wearmemory_audio_v166/localonly_patch.py
+++ wearmemory_audio_v166/localonly_patch.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
 
-root = Path(os.environ.get('WM_AUDIO_ROOT', '/tmp/wm166'))
+root = Path(os.environ.get('WM_AUDIO_ROOT', '/tmp/wm167' if Path('/tmp/wm167').exists() else '/tmp/wm166'))
 appmodel = root / 'WearMemory/AppModel.swift'
 project = root / 'project.yml'
 info = root / 'WearMemory/Info.plist'
