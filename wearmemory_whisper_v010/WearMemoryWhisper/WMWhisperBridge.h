9697c2bf188ebe561b5b6cd0091f3f1b6f8aa073
shtorm095 <shtorm095@gmail.com>
2026-08-22 19:22:43 +0200

    Whisper build 5: split context init from inference

--- wearmemory_whisper_v010/WearMemoryWhisper/WMWhisperBridge.h
+++ wearmemory_whisper_v010/WearMemoryWhisper/WMWhisperBridge.h
@@ -8,12 +8,17 @@ int wm_whisper_test_model(const char * _Nonnull modelPath,
                           double * _Nullable elapsedSeconds,
                           char * _Nullable * _Nullable errorOut);
 
-const char * _Nullable wm_whisper_transcribe(const char * _Nonnull modelPath,
-                                              const float * _Nonnull samples,
-                                              int sampleCount,
-                                              int threads,
-                                              double * _Nullable elapsedSeconds,
-                                              char * _Nullable * _Nullable errorOut);
+void * _Nullable wm_whisper_create_context(const char * _Nonnull modelPath,
+                                            char * _Nullable * _Nullable errorOut);
+
+const char * _Nullable wm_whisper_transcribe_context(void * _Nonnull context,
+                                                      const float * _Nonnull samples,
+                                                      int sampleCount,
+                                                      int threads,
+                                                      double * _Nullable elapsedSeconds,
+                                                      char * _Nullable * _Nullable errorOut);
+
+void wm_whisper_free_context(void * _Nullable context);
 void wm_whisper_free_string(const char * _Nullable value);
 
 #ifdef __cplusplus
