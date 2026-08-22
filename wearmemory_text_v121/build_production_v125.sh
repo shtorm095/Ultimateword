a9a1d30ebe659f748a3b42f4fc33e0cd66601cc6
shtorm095 <shtorm095@gmail.com>
2026-08-23 01:02:26 +0200

    Fix Text 1.2.5 build wrapper

--- wearmemory_text_v121/build_production_v125.sh
+++ wearmemory_text_v121/build_production_v125.sh
@@ -34,23 +34,6 @@ source = source.replace(
     "source = source.replace('WearMemoryText_v1.2.1_Source.zip', 'WearMemoryText_v1.2.5_Source.zip')",
 )
 
-verify_marker = '''    + "test -f \\\"$SRC/GermanTranscriptNormalizer.swift\\\"\\n"
-'''
-if verify_marker not in source:
-    raise SystemExit('v1.2.4 verification tail marker not found')
-verify_extra = verify_marker \
-    + '''    + "test -f \\\"$SRC/BackgroundExecutionKeeper.swift\\\"\\n" \\
-''' \
-    + '''    + "grep -Fq 'processor.$isProcessing' \\\"$SRC/TextAppModel.swift\\\"\\n" \\
-''' \
-    + '''    + "grep -Fq 'UIBackgroundModes' \\\"$SRC/Info.plist\\\"\\n" \\
-''' \
-    + '''    + "grep -Fq '<string>audio</string>' \\\"$SRC/Info.plist\\\"\\n" \\
-''' \
-    + '''    + "! grep -Fq 'phase == .background { model.stopPolling() }' \\\"$SRC/WearMemoryTextApp.swift\\\"\\n"
-'''
-source = source.replace(verify_marker, verify_extra, 1)
-
 out.write_text(source)
 PY
 
